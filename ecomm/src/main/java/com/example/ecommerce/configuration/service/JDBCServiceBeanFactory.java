c61bc275a772e8c1f601e8d217b75a54a7bddf3c
shaan <shaanmohd372@gmail.com>
2025-04-19 23:07:18 +0530

    Made changes

--- ecomm/src/main/java/com/example/ecommerce/configuration/service/JDBCServiceBeanFactory.java
+++ ecomm/src/main/java/com/example/ecommerce/configuration/service/JDBCServiceBeanFactory.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name = "spring.datasource.access-mode", havingValue = "jdbc")
+@ConditionalOnProperty(name = "spring.datasource.access-mode",
+		havingValue = "jdbc")
 public class JDBCServiceBeanFactory {
 
 	@Bean
